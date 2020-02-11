59154a523b70098afd492a8e8fa0382e5363a0e4
Nikolay Bratoev <49118122+nikolaybratoev@users.noreply.github.com>
2020-02-11 22:23:54 +0200

    Update StringMatrixRotation.java

--- Java-Advanced/src/multidimensional_arrays/StringMatrixRotation.java
+++ Java-Advanced/src/multidimensional_arrays/StringMatrixRotation.java
@@ -1,5 +1,36 @@
 package multidimensional_arrays;
 
+
+/*
+String Matrix Rotation
+
+You are given a sequence of text lines. Assume these text lines form a matrix of characters 
+(pad the missing positions with spaces to build a rectangular matrix). Write a program to rotate
+the matrix by 90, 180, 270, 360, … degrees. Print the result at the console as sequence of strings
+after receiving the “END” command.
+
+INPUT
+The input is read from the console:
+    •	The first line holds a command in format "Rotate(X)" where X are the degrees of the requested rotation
+    •	The next lines contain the lines of the matrix for rotation.
+    •	The input ends with the command "END".
+    
+OUTPUT
+Print at the console the rotated matrix as a sequence of text lines.
+
+Examples
+
+INPUT           OUTPUT   |   INPUT          OUTPUT
+Rotate(90)      esh      |   Rotate(810)    ej
+hello           xoe      |   js             xs
+softuni         afl      |   exam           a
+exam            mtl      |   END            m
+END              uo      |
+                 n       |
+                 i       |
+
+*/
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
