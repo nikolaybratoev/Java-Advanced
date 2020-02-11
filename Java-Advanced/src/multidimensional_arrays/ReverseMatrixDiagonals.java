c210800bca4e5c9bb6fff389f2c906d979579bd5
Nikolay Bratoev <49118122+nikolaybratoev@users.noreply.github.com>
2020-02-11 22:06:15 +0200

    Update ReverseMatrixDiagonals.java

--- Java-Advanced/src/multidimensional_arrays/ReverseMatrixDiagonals.java
+++ Java-Advanced/src/multidimensional_arrays/ReverseMatrixDiagonals.java
@@ -1,5 +1,20 @@
 package multidimensional_arrays;
 
+//Reverse Matrix Diagonals
+
+/*
+You are given a matrix (2D array) of integers. 
+You have to print the matrix diagonal but in reversed order. Print each diagonal on new line. 
+
+INPUT
+On the first line, single integer the number R of rows in the matrix. On each of the next R lines, 
+C numbers separated by single spaces. Note that R and C may have different values. 
+
+OUTPUT
+The output should consist of R lines, each consisting of exactly C characters, separated by spaces,
+representing the matrix diagonals reversed.
+*/
+
 import java.util.Arrays;
 import java.util.Scanner;
 
